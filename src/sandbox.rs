c3c72eda7156b89ffec61d8f2d35bf57d6bb6936
malt03 <k@malt03.com>
2024-04-28 19:02:48 +0900

    実装の整理

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -24,22 +24,13 @@ impl Sandbox {
         unduplicate_elements(x.iter()).collect()
     }
 
-    // pub fn forward_without_train(&self, inputs: &[f64]) -> Vec<f64> {
-    //     let x = duplicate_elements(inputs.into_iter()).collect();
-    //     let x = self.layer0.forward_without_train(x);
-    //     let x = self.last_layer.forward_without_train(x);
-    //     unduplicate_elements(x.iter()).collect()
-    // }
-
     pub fn backward(&mut self, delta: Vec<f64>) {
         let delta = duplicate_elements(delta.iter()).collect();
-        let delta = self.last_layer.backward_multi(&delta);
-
-        // for d in unduplicate_elements(delta.iter()) {
-        //     self.layer0.backward(d);
-        // }
+        self.last_layer.backward_multi(&delta);
     }
 }
+
+const LEARNING_RATE: f64 = 0.8;
 
 fn main() {
     let mut model = Sandbox::new();
@@ -51,13 +42,13 @@ fn main() {
         (vec![1., 1.], vec![1., 0.]),
     ];
 
-    for _ in 0..10000 {
+    for _ in 0..1000 {
         let mut sum_loss = 0.;
         for (input, target) in train.iter() {
             let output = model.forward(&input);
             let loss = CrossEntropyLoss::eval((&output, &target));
             let delta = CrossEntropyLoss::derivative((&output, &target));
-            model.backward(delta.into_iter().map(|d| d * 0.2).collect());
+            model.backward(delta.into_iter().map(|d| d * LEARNING_RATE).collect());
 
             sum_loss += loss;
         }
